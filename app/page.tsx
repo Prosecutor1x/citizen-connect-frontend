60f4e2452a33c6788c1ec725e0d754f24399dd32
Prosecutor1x <krishnarjunnanda@gmail.com>
2023-06-02 21:11:17 +0530

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center ">
+     <div className='text-5xl font-semibold '>Welcome to Citizen Connect</div>
+    </div>
+  )
+}
+y
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontSize: {
+        xs: '10px',
+        sx: '12px',
+        sm: '14px',
+        base: '16px',
+        lg: '18px',
+        xl: '20px',
+        '1.5xl': '24px',
+        '5xl': '64px',
+        '2xl': '32px',
+        '3xl': '40px',
+        '4xl': '48px',
+        '5xl': '60px',
+        '6xl': '72px',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1108px',
+        xl: '1224px',
+        '2xl': '1430px',
+        '3xl': '1670px',
+      },
+      colors: {
+       
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        quicksand: ['Quicksand', 'Sans-Serif'],
+      },
+    },
+  },
+  plugins: [],
+}
