a1607761a24397b3d52db3fab2ca58814e6beb5c
kingshuk3006 <kingsarkar3006@gmail.com>
2023-06-30 11:21:40 +0530

    sdvs

---
+++ app/profile-create/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const CreateProfile= () => {
+  return (
+    <div className='bg-blueBackground'>
+        <Navbar/>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default CreateProfile
