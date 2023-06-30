824513eb286eabc89d3c3f99de1551f8e4cbf02c
Kinghshuk Sarkar <kinghshuksarkar@Kinghshuks-MacBook-Air.local>
2023-07-01 01:12:39 +0530

    upto profile

--- components/IssueReport.tsx/issueReport.Card.tsx
+++ components/IssueReport.tsx/issueReport.Card.tsx
@@ -2,17 +2,13 @@ import React from 'react'
 
 const IssueReportCard = () => {
     return (
-        <div className='max-w-[20rem] bg-bluePrimary p-3 rounded-3xl shadow-xl'>
+        <div className='max-w-[20rem] bg-white py-3 rounded-2xl shadow-lg hover:scale-105 transition-all duration-200'>
             <div className=' w-full flex justify-center items-center '>
-                <span className='px-6 py-1  rounded-full bg-white -mt-10 border-2  border-bluePrimary'>Step 1</span>
+                <span className='px-6 py-1  rounded-full bg-bluePrimary text-white -mt-10'>Step 1</span>
             </div>
-            <div className='w-full h-full bg-white rounded-2xl'>
-
-                <img src='/search.png' />
-                <h1 className='bg-bluePrimary text-white px-6 py-2 font-semibold text-center'>Find a issue in your locality</h1>
-                <h2 className='px-6 py-2 text-center'>Report your problems in your locality to the government</h2>
-            </div>
-
+            <img src='/search.png' />
+            <h1 className='bg-bluePrimary text-white px-6 py-4 text-lg font-semibold text-center'>Find a issue in your locality</h1>
+            <h2 className='px-6 py-3 text-center'>Report your problems in your locality to the government</h2>
         </div>
     )
 }
---
+++ components/profile/AccountDetails.tsx
@@ -0,0 +1,91 @@
+import { Avatar, Input, Select } from '../../app/lib/chakraui'
+import React from 'react'
+
+const AccountDetails = () => {
+    return (
+        <div className='space-y-6'>
+            <section className='flex justify-start space-x-4 items-center '>
+                <Avatar name='kingshuk Sarkar' size='2xl' />
+                <div>
+                    <h1 className='text-2xl font-semibold'>Kingshuk Sarkar</h1>
+                </div>
+            </section>
+            <div className='grid grid-cols-5 gap-4 place-content-center items-center'>
+                <section className='col-span-2'>
+                    <h1 className='text-xl font-medium mb-1'>Email id</h1>
+                    <p className='text-base font-light text-[#00184485]'>All of your document and messages will be sent via this email id</p>
+                </section>
+                <section className='col-span-3'>
+                    <Input
+                        type="text"
+                        backgroundColor={'#FBFAFF'}
+                        focusBorderColor="#1A75FF"
+                        placeholder="abc@gmail.com"
+                        size={'md'}
+                        fontSize="base"
+                    />
+                </section>
+            </div>
+            <hr className='my-2' />
+            <div className='grid grid-cols-5 gap-4 place-content-center items-center'>
+                <section className='col-span-2'>
+                    <h1 className='text-xl font-medium mb-1'>Gender</h1>
+                </section>
+                <section className='col-span-3'>
+                    <Select
+                        backgroundColor={'#FBFAFF'}
+                        placeholder='Gender'
+                        focusBorderColor="#1A75FF"
+                        size={'md'}
+                        value={'Male'}
+                        fontSize="base">
+                        <option>
+                            Male
+                        </option>
+                        <option>
+                            Female
+                        </option>
+                        <option>
+                            Trans
+                        </option>
+
+                    </Select>
+                </section>
+            </div>
+            <hr className='my-2' />
+            <div className='grid grid-cols-5 gap-4 place-content-center items-center'>
+                <section className='col-span-2'>
+                    <h1 className='text-xl font-medium mb-1'>Age</h1>
+                </section>
+                <section className='col-span-3'>
+                    <Input
+                        type="text"
+                        backgroundColor={'#FBFAFF'}
+                        focusBorderColor="#1A75FF"
+                        placeholder="Enter Age"
+                        size={'md'}
+                        fontSize="base"
+                    />
+                </section>
+            </div>
+            <hr className='my-2' />
+            <div className='grid grid-cols-5 gap-4 place-content-center items-center'>
+                <section className='col-span-2'>
+                    <h1 className='text-xl font-medium mb-1'>Locality</h1>
+                </section>
+                <section className='col-span-3'>
+                    <Input
+                        type="text"
+                        backgroundColor={'#FBFAFF'}
+                        focusBorderColor="#1A75FF"
+                        placeholder="Enter City/Town/Village"
+                        size={'md'}
+                        fontSize="base"
+                    />
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default AccountDetails
